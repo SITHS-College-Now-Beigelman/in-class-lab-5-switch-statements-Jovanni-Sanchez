c0603d1fab42a26f643727dd27c279ebc232e877
Jovanni <jovannis8@nycstudents.net>
2024-10-08 21:38:09 -0400

    This is a start
    I will do the rest later

--- Sanchez_Jovanni_Lab_5_Part_3-1.cpp
+++ Sanchez_Jovanni_Lab_5_Part_3-1.cpp
@@ -7,48 +7,51 @@
 #include <iostream>
 using namespace std;
 
-//1 means North, 2 means South, 3 means East and 4 means West
+// 1 means North, 2 means South, 3 means East and 4 means West
 
 int main()
 {
-    int pleaseTallyNorth = 0;
-    int pleaseTallySouth = 0;
-    int pleaseTallyEast = 0;
-    int pleaseTallyWest = 0;
-    int possibleRobotMoves;
+    int currentRobotStep;
+    int numOfStepsEast = 0;
+    int numOfStepsWest = 0;
+    int numOfStepsNorth = 0;
+    int numOfStepsSouth = 0;
 
     srand(time(0));
 
-    for (possibleRobotMoves = 0; possibleRobotMoves < 25; possibleRobotMoves++)
+    cout << "The robot will take these steps:\n"
+         << "\n";
+
+    for (currentRobotStep = 0; currentRobotStep < 25; currentRobotStep++)
     {
         int internalDirection = rand() % 4 + 1;
 
         switch (internalDirection)
         {
-            case 1:
-                cout << "North.\n";
-                pleaseTallyNorth += 1;
-                break;
-            case 2:
-                cout << "South.\n";
-                pleaseTallySouth += 1;
-                break;
-            case 3:
-                cout << "East.\n";
-                pleaseTallyEast += 1;
-                break;
-            case 4:
-                cout << "West.\n";
-                pleaseTallyWest += 1;
-                break;
+        case 1:
+            cout << "North.\n";
+            numOfStepsNorth += 1;
+            break;
+        case 2:
+            cout << "South.\n";
+            numOfStepsSouth += 1;
+            break;
+        case 3:
+            cout << "East.\n";
+            numOfStepsEast += 1;
+            break;
+        case 4:
+            cout << "West.\n";
+            numOfStepsWest += 1;
+            break;
         }
     }
-    
-    cout << "\nAfter 25 Steps, The robot is:\n"
-         << pleaseTallyNorth << " steps North.\n"
-         << pleaseTallySouth << " steps South.\n"
-         << pleaseTallyEast << " steps East.\n"
-         << pleaseTallyWest << " steps West.\n";
+
+    cout << "\nAfter 25 Steps, The robot took:\n"
+         << numOfStepsNorth << " steps North.\n"
+         << numOfStepsSouth << " steps South.\n"
+         << numOfStepsEast << " steps East.\n"
+         << numOfStepsWest << " steps West.\n";
 
     return 0;
 }
--- Sanchez_Jovanni_Lab_5_Part_3-2.cpp
+++ Sanchez_Jovanni_Lab_5_Part_3-2.cpp
@@ -12,5 +12,40 @@ using namespace std;
 
 int main()
 {
+    int numOfStepsEast = 0;
+    int numOfStepsWest = 0;
+    int numOfStepsNorth = 0;
+    int numOfStepsSouth = 0;
+    int totalNumberOfSteps = 0;
+
+    srand(time(0));
+
+    while (numOfStepsEast != 2 && numOfStepsNorth != 3)
+    {
+        int internalDirection = rand() % 4 + 1;
+
+        switch (internalDirection)
+        {
+        case 1:
+            cout << "North.\n";
+            numOfStepsNorth += 1;
+            totalNumberOfSteps += 1;
+            break;
+        case 2:
+            cout << "South.\n";
+            totalNumberOfSteps += 1;
+            break;
+        case 3:
+            cout << "East.\n";
+            numOfStepsEast += 1;
+            totalNumberOfSteps += 1;
+            break;
+        case 4:
+            cout << "West.\n";
+            totalNumberOfSteps += 1;
+            break;
+        }
+    }
+
     return 0;
 }
