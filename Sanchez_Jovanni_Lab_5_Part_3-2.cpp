1805b1415b3bfa97b47fe9c4605623dc2e1bb7d0
Jovanni <jovannis8@nycstudents.net>
2024-10-12 18:56:35 -0400

    i'll write more comments later

--- Sanchez_Jovanni_Lab_5_Part_3-2.cpp
+++ Sanchez_Jovanni_Lab_5_Part_3-2.cpp
@@ -3,6 +3,7 @@
 // Problem #2
 // 10/8/2024
 
+#include <array> // Averaging is a pain
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
@@ -10,5 +11,75 @@ using namespace std;
 
 int main()
 {
+    int currentLoop;
+    int numOfStepsEast = 0;
+    int numOfStepsNorth = 0;
+    int sumOfRobotSteps = 0;
+    int avgNumberOfSteps = 0;
+    array<float, 10> everyPossibleStep;
+
+    srand(time(0));
+
+    for (currentLoop = 0; currentLoop < 10; currentLoop++)
+    {
+        bool roaming = true;
+        int tmpTotalSteps = 0;
+
+        while (roaming)
+        {
+            int internalDirection = rand() % 4 + 1;
+
+            cout << left;
+
+            switch (internalDirection)
+            {
+            case 1:
+                cout << "North.\n";
+                numOfStepsNorth += 1;
+                tmpTotalSteps += 1;
+                break;
+            case 2:
+                cout << "South.\n";
+                numOfStepsEast = 0;
+                numOfStepsNorth = 0;
+                tmpTotalSteps += 1;
+                break;
+            case 3:
+                cout << "East.\n";
+                numOfStepsEast += 1;
+                tmpTotalSteps += 1;
+                break;
+            case 4:
+                cout << "West.\n";
+                numOfStepsEast = 0;
+                numOfStepsNorth = 0;
+                tmpTotalSteps += 1;
+                break;
+            }
+
+            if (numOfStepsEast == 2 && numOfStepsNorth == 3)
+            {
+                roaming = false;
+                everyPossibleStep[currentLoop] = tmpTotalSteps;
+                cout << "The robot has reached it's destination after " << tmpTotalSteps << " steps.\n";
+            }
+            else
+            {
+                roaming = true;
+            }
+        }
+    }
+
+    for (currentLoop = 0; currentLoop < everyPossibleStep.size(); currentLoop++)
+    {
+        sumOfRobotSteps = sumOfRobotSteps + everyPossibleStep[currentLoop]; // get sum of the total of each loop
+    }
+
+    avgNumberOfSteps = sumOfRobotSteps / 10;
+
+    cout << "\nAfter 10 loops, the robot has made:\n"
+         << sumOfRobotSteps << " total steps or an average of "
+         << avgNumberOfSteps << " steps per loop.";
+
     return 0;
 }
