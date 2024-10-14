aac78c787bcb25dc66ba0af1913c9688176690e3
Jovanni <jovannis8@nycstudents.net>
2024-10-14 12:04:40 -0400

    rip arrays, you will be missed
    the array was only being used once. there was no real need of storing the individual total scores as they were already being printed and added to a sum, so I just delete the array and reused the sumOfEveryStepTaken varaible.

--- Sanchez_Jovanni_Lab_5_Part_3-2.cpp
+++ Sanchez_Jovanni_Lab_5_Part_3-2.cpp
@@ -3,7 +3,6 @@
 // Problem #2
 // 10/8/2024
 
-#include <array>
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
@@ -24,7 +23,6 @@ int main()
     int numOfStepsNorth = 0;
     int avgNumberOfSteps = 0;
     int sumOfEveryStepTaken = 0;
-    array<float, 10> everyPossibleStep; // el diablo >:(
 
     srand(time(0)); // Real time changes (woah :O)
 
@@ -38,19 +36,11 @@ int main()
          << "   - It has consecutively made 2 steps East and 3 steps North.\n"
          << "   - Or it dies :c\n\n";
 
-    /* I hate using arrays, it's awesome. Arrays are a necessary evil in this instance
-    (at least i think they are...) I not only have to make the code run until the robot
-    consecutively makes 2 steps east and 3 steps north (in no particular order), but also
-    make the robot do it TEN TIMES and get an average out of it. So I used the help of an array
-    (grr >:c) to store the total after each loop to use them later to find a sum and a average. I
-    was thinking to use a regular variable but I am either to far gone or stupid to realize I don't
-    need arrays. */
-
     for (currentLoop = 0; currentLoop < 10; currentLoop++)
     {
         bool roaming = true;
         int tmpTotalSteps = 0;
-        int cuttingOffPoint = 840; // sorry lil buddy
+        int cuttingOffPoint = 900; // sorry lil buddy
 
         /* Here I assign temporary variables only used within this for and while loop.
         The bool roaming is used to determine wheter the robot has reached it's destination
@@ -58,8 +48,7 @@ int main()
         of each steps of each loop, which will then be added to the array for safe keeping. The
         cuttingOffPoint is exactly what it sounds like. I was thinking after surpassing a limit of
         steps (probably a high one), I kill the robot and set the total for that loop to the cutting
-        off point. I decide not to implement the cutting off point as I couldn't think off a good limit
-        to just end the robot's run. */
+        off point. */
 
         while (roaming)
         {
@@ -93,20 +82,21 @@ int main()
             }
 
             /* If the robot has reached it's destination, I tell it to stop roaming
-            and print out the total number of steps it took to get their. Else if, the robot
-            exceeds the limit of steps it can take, I cut it off and set the total for that loop
-            to the limit. Else, I make it keep roaming until it eventually reaches it's destination. */
+            and print out the total number of steps it took to get there alongside adding the total
+            to the sum. Else if, the robot exceeds the limit of steps it can take, I cut it off and set
+            the total for that loop to the limit. Else, I make it keep roaming until it eventually reaches
+            it's destination. */
 
             if (numOfStepsEast == 2 && numOfStepsNorth == 3)
             {
                 roaming = false;
-                everyPossibleStep[currentLoop] = tmpTotalSteps;
+                sumOfEveryStepTaken = sumOfEveryStepTaken + tmpTotalSteps;
                 cout << "The robot has reached it's destination after " << tmpTotalSteps << " steps.\n";
             }
             else if (tmpTotalSteps >= cuttingOffPoint)
             {
                 roaming = false;
-                everyPossibleStep[currentLoop] = cuttingOffPoint;
+                sumOfEveryStepTaken = sumOfEveryStepTaken + tmpTotalSteps;
                 cout << "The robot has reached it's destination after " << cuttingOffPoint << " steps.\n";
             }
             else
@@ -116,16 +106,9 @@ int main()
         }
     }
 
-    /* The code below is really simple. First, I use a for loop to assign the sum of every
-    step taken in the above loops to the variable sumOfEveryStepTaken by just skimming through
-    the list and adding it's totals to the variable. Next I assign avgNumberOfSteps to that sum
+    /* The code below is really simple. First I assign avgNumberOfSteps to the sum
     divide by the numbers of loops the robot had (10). Finally I output that sum and average in
     a nice little sentence. */
-
-    for (currentLoop = 0; currentLoop < everyPossibleStep.size(); currentLoop++)
-    {
-        sumOfEveryStepTaken = sumOfEveryStepTaken + everyPossibleStep[currentLoop];
-    }
 
     avgNumberOfSteps = sumOfEveryStepTaken / 10;
 
